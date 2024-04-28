ff91950cf98502abbd5773d7ff196b8bac097b15
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-04-28 20:36:08 +0800

    feat: add truncate if title is too long

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types'
 
 function Card({ title, img, url }) {
+
+    const truncate = (str, limit) => {
+        if(str.length > limit) {
+            return str.substring(0, limit) + "..."
+        }
+        return str
+    }
+
     return(
         <a target='_blank' href={url} className="duration-200 max-sm:max-w-32 group w-full relative max-w-52 dark:bg-neutral-700 bg-neutral-200 rounded-md flex flex-col overflow-hidden border-2 dark:border-neutral-500">
             <img className='w-full aspect-[4/5] object-fit object-center' src={img} alt="" />
-            <h3 className='group-hover:opacity-100 duration-200 opacity-0 absolute left-0 bottom-0 bg-black/90 w-full p-2 text-base text-center'>{title}</h3>
+            <h3 className='group-hover:opacity-100 duration-200 opacity-0 absolute left-0 bottom-0 bg-black/90 w-full p-2 text-base text-center' title={title}>{truncate(title,50)}</h3>
         </a>
     )
 }
