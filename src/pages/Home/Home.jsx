edf5de6df77763966f91212dfc2f9b97836cfbf8
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-04-28 19:15:00 +0800

    refactor: update search border in darkmode

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -74,7 +74,7 @@ function Home() {
         <>
             <div className="w-full dark:bg-black duration-200 bg-white flex flex-col gap-5 py-10">
                 <div className="w-full duration-200 dark:bg-black bg-white max-w-[1200px] mx-auto flex justify-center items-center">
-                    <div className="border-neutral-300 duration-200 w-full max-w-[400px] rounded-full border-2 py-2 px-3 flex justify-between items-center">
+                    <div className="border-neutral-300 dark:border-neutral-700 duration-200 w-full max-w-[400px] rounded-full border-2 py-2 px-3 flex justify-between items-center">
                         <input onInput={search} placeholder="Type to search..." className="dark:text-white text-black peer w-full bg-transparent focus:outline-none" type="text" name="" id="" />
                         <svg className="peer-focus:text-neutral-100 duration-200 text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24"><path className="text-inherit" fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"/></svg>
                     </div>
